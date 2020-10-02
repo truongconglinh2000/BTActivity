c4becba8ff8a7ba2151bcd46b420b5b096c30ba2
truongconglinh2000 <71702117+truongconglinh2000@users.noreply.github.com>
2020-10-02 22:45:44 +0700

    Activity

---
+++ BTTuan4/app/src/main/java/com/example/bttuan4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bttuan4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnOpen=(Button)
+                findViewById(R.id.button);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+                                               public void onClick(View arg0) {
+                                                   doOpenChildActivity();
+                                               }
+                                           });
+    }
+    public void doOpenChildActivity()
+    {
+        Intent myIntent=new Intent(this, ChildActivity.class);
+        startActivity(myIntent);
+    }
+
+}
